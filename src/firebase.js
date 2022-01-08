b73676df15ccb7bc51c23afe86b44295138f97ec
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-08 22:38:35 +0100

    create env file

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuSH71pF6g1MPBIoXw9HacVaGBYbW3gGc",
+  authDomain: "react-messenger-decb1.firebaseapp.com",
+  databaseURL: "https://react-messenger-decb1.firebaseio.com",
+  projectId: "react-messenger-decb1",
+  storageBucket: "react-messenger-decb1.appspot.com",
+  messagingSenderId: "648854951266",
+  appId: "1:648854951266:web:46e86d1014c5d32850c83b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
