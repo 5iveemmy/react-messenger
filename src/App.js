2145b306cb17379ca836aa58ebe64c2edb64471c
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-10 12:13:01 +0100

    create home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
