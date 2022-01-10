5f3a8ff7781fa82dc58c67daa5da4ea4dea7e1e3
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-10 12:35:08 +0100

    router bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route path="/" exact component={Home} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
