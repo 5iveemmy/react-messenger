5aca85cbf788b07f976e92e564fb27f9db19d755
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-11 02:17:37 +0100

    create register file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="register" element={<Register />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
