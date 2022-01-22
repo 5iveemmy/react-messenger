9eb0a842e9f85f452720ccda69d05cab9676cd3b
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-22 09:37:33 +0100

    imports from firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { db, auth } from "../firebase";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
 
 function Home() {
-  return (
-    <div className="home">
-      <h1>Home Page</h1>
-    </div>
-  );
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    // create query object
+    const q = query(usersRef, where("uid"));
+  });
+  return <div> Home Page</div>;
 }
 
 export default Home;
