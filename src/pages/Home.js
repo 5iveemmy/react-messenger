c5f74d275e02022064eb2154b2e96ee0337f1765
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-01-23 22:22:28 +0100

    add handleSumit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { db, auth } from "../firebase";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 import User from "../components/User";
 import MessageForm from "../components/MessageForm";
 
 function Home() {
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState("");
+  const [text, setText] = useState("");
 
+  const user1 = auth.currentUser.uid;
   useEffect(() => {
     const usersRef = collection(db, "users");
     // create query object
-    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+    const q = query(usersRef, where("uid", "not-in", [user1]));
     // execute query
     const unsub = onSnapshot(q, (querySnapshot) => {
       let users = [];
@@ -26,6 +34,17 @@ function Home() {
     setChat(user);
     console.log(user);
   };
+
+  const hnadleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user2 = chat.uid;
+
+    const id = user1 > user2 ? `${user1 + user2}` : `{user2 + user1}`;
+
+    await addDoc(collection(db, "messages", id, "chat"));
+  };
+
   return (
     <div className="home_container">
       <div className="users_container">
